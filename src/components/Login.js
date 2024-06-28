012be5a0310948281f7b9ed4d5064c1698149f4c
Arpit Rathi <rathiarpit29@gmail.com>
2024-06-28 11:33:59 +0530

    added form validation for singin/signup form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,33 +1,50 @@
 import Header from "./Header";
 import { BACKGROUND_IMG } from "../utils/constants";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import {checkValidData } from "./../utils/validate";
 
 
 const Login = () => {
     const [isSignInForm , setIsSignInForm ] = useState(true);
+    const [errorMssg,setErrorMssg] = useState(null);
+
+    const email = useRef(null);
+    const password = useRef(null);
 
     const toggleSignIn = () => {
         setIsSignInForm(!isSignInForm);
     }
 
+    const handleClick = () => {
+        const emailValue = email.current.value;
+        const passwordValue = password.current.value;
+        const errorMssg = checkValidData(emailValue,passwordValue);
+        setErrorMssg(errorMssg);
+    }
+
     return(
         <div className="flex justify-center align-middle">
             <Header />
+
             <div className="absolute top-0 bg-black ">
                 <img className="opacity-50"  src={BACKGROUND_IMG}></img>
             </div>
-            <form className="w-1/4  relative p-12 bg-black flex flex-col mt-40 opacity-80 text-white rounded-md">
+
+            <form  onSubmit={(e)=> {e.preventDefault() }} className="w-1/4  relative p-12 bg-black flex flex-col mt-40 opacity-80 text-white rounded-md">
+
                 <div className="m-2 " style={{fontSize:'2rem',fontWeight:'700',color:'#FFFFFF'}}>
                     { isSignInForm ? 'Sign In': 'Sign Up'}</div>
                 
                 {!isSignInForm && <input  className="p-4 m-4 rounded-md bg-gray-700"   type="text" placeholder="Full Name"></input>}
 
-                <input  className="p-4 m-4 rounded-md bg-gray-700"   type="text" placeholder="Email or mobile number"></input>
+                <input ref={email} className="p-4 m-4 rounded-md bg-gray-700"   type="text" placeholder="Email or mobile number"></input>
                 
-                <input  className="p-4 m-4 rounded-md bg-gray-700" type="password" placeholder="Password"></input>
+                <input ref={password} className="p-4 m-4 rounded-md bg-gray-700" type="password" placeholder="Password"></input>
                 
-                <button className="p-2 m-4 bg-red-700 rounded-md flex justify-center text-white opacity-100 " style={{textAlign:'center'}} >
-                    { isSignInForm ? 'Sign In' : 'Sign Up'}</button>
+                { errorMssg && <p className="p-2 mx-4 text-red-700 font-bold text-lg"> {errorMssg} </p> }
+                
+                <button className="p-2 m-4 bg-red-700 rounded-md flex justify-center text-white opacity-100 "
+                 style={{textAlign:'center'}}  onClick={handleClick} >  { isSignInForm ? 'Sign In' : 'Sign Up'}</button>
 
                 <p className="p-4 cursor-pointer" onClick={ toggleSignIn }>
                     {isSignInForm ? 'New to Netflix? Sign Up Now' : 'Already registered? Sign In Now.'}</p>
